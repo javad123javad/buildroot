7765972bfa5910767bb61118ca772f147fd4edc2
Javad Rahimi <javad321javad@gmail.com>
2022-03-27 11:46:26 +0430

    Installation script added to the packages

--- package/unity/unity.mk
+++ package/unity/unity.mk
@@ -8,8 +8,12 @@ UNITY_VERSION = b9e9268d92e5339e8cdd251c880429ce30ff7a11
 UNITY_SITE = https://github.com/ThrowTheSwitch/Unity.git
 UNITY_SITE_METHOD = git
 UNITY_INSTALL_STAGING = YES
-UNITY_INSTALL_TARGET = NO
+UNITY_INSTALL_TARGET = YES
 UNITY_CONF_OPTS = -DBUILD_DEMOS=ON
 UNITY_DEPENDENCIES = host-pkgconf
 
+#define UNITY_INSTALL_TARGET_CMDS
+# $(INSTALL) -m 0755 -D $(@D)/libunity.a $(TARGET_DIR)/usr/bin/fbgrab
+#endef
 $(eval $(cmake-package))
+
