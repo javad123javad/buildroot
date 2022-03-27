b97e5e2751850e292ac08bb68c9ed335d8e13bb5
Javad Rahimi <javad321javad@gmail.com>
2022-03-27 15:09:33 +0430

    Some cleaning

--- package/clay/clay_assignment.mk
+++ package/clay/clay_assignment.mk
@@ -7,7 +7,7 @@
 CLAY_VERSION = 46fc4388354ba735c5a51db7a6e35e1afe25f630 #194d4384ffc1458d98eb38f2ebeb48bdd994df15
 CLAY_SITE = https://github.com/javad123javad/em_param_server.git
 CLAY_SITE_METHOD = git
-CLAY_INSTALL_STAGING = YES 
+#CLAY_INSTALL_STAGING = YES 
 CLAY_INSTALL_TARGET = YES 
 CLAY_CONF_OPTS = -DBUILD_DEMOS=ON
 #CLAY_DEPENDENCIES = allib  host-pkgconf
@@ -15,8 +15,9 @@ CLAY_CONF_OPTS = -DBUILD_DEMOS=ON
 define CLAY_INSTALL_STAGING_CMDS
     $(INSTALL) -D -m 0755 $(@D)/Clay_Assignment  $(TARGET_DIR)/usr/bin/Clay_Assignment
 endef
-#define CLAY_INSTALL_TARGET_CMDS
-#    $(INSTALL) -D -m 0755 $(@D)/Clay_Assignment  $(TARGET_DIR)/usr/bin/Clay_Assignment
+define CLAY_INSTALL_TARGET_CMDS
+    echo $(@D)
+    $(INSTALL) -D -m 0755 $(@D)/Clay_Assignment  $(TARGET_DIR)/usr/bin/Clay_Assignment
 
-#endef
+endef
 $(eval $(cmake-package))
