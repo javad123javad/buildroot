92d07b2169b7d44eeaf33e72d4c42c85a5ede239
Javad Rahimi <javad321javad@gmail.com>
2022-03-28 07:26:09 +0430

    git release added

--- package/clay2/clay2.mk
+++ package/clay2/clay2.mk
@@ -4,20 +4,20 @@
 #
 ################################################################################
 
-CLAY2_VERSION = 1.0 
-CLAY2_SITE = ./package/clay2/src
-CLAY2_SITE_METHOD = local
-#CLAY_INSTALL_STAGING = YES
-#CLAY_INSTALL_TARGET = YES 
+CLAY2_VERSION = 1.1
+CLAY2_SOURCE = clay2-v1.1.tar.gz 
+
+CLAY2_SITE = https://github.com/javad123javad/em_param_server/archive/refs/tags #./package/clay2/src
+CLAY2_SITE_METHOD = wget #local
 CLAY2_AUTORECONF = YES
 CLAY2_LICENSE = GPL-3.0+
 CLAY2_LICENSE_FILES = COPYING
 CLAY2_CPE_ID_VENDOR = gnu
-#CLAY2_INSTALL_STAGING = YES
 CLAY2_INSTALL_TARGET = YES
 
 define CLAY2_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/src/Clay_Assignment  $(TARGET_DIR)/usr/bin
 endef
+
 $(eval $(autotools-package))
 
